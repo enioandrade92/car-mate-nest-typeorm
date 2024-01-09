c4b1850890f7600f1729e5296d439667875b90c9
Enio Andrade <enio.goulart@gmail.com>
2024-01-09 15:59:02 -0300

    feat(driver) generated driver module

---
+++ src/modules/vehicle/entities/vehicle.entity.ts
@@ -0,0 +1,35 @@
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('vehicles')
+export class Vehicle {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('text', { default: null })
+    name: string;
+
+    @Column('text', { default: null })
+    color: string;
+
+    @Column('text', { default: null })
+    brand: string;
+
+    @Column('text', { default: null })
+    plate: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @DeleteDateColumn({ default: null })
+    deletedAt: Date;
+}
