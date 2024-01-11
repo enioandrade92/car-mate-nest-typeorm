e92892b60d4b56d31b7fff702805b56cc39cae50
Enio Andrade <enio.goulart@gmail.com>
2024-01-11 14:13:56 -0300

    feat(assignment-test): implemented controller tests

--- test/unit/modules/vehicle-assignment/vehicle-assignment.controller.spec.ts
+++ test/unit/modules/vehicle-assignment/vehicle-assignment.controller.spec.ts
@@ -1,20 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { VehicleAssignmentController } from './vehicle-assignment.controller';
-import { VehicleAssignmentService } from './vehicle-assignment.service';
+import { VehicleAssignmentController } from '../../../../src/modules/vehicle-assignment/vehicle-assignment.controller';
+import { VehicleAssignmentService } from '../../../../src/modules/vehicle-assignment/vehicle-assignment.service';
+import { mockAssignmentService } from './mock-methods';
+import { mockCreateRegister, mockRegisters } from './mock-data';
 
 describe('VehicleAssignmentController', () => {
-  let controller: VehicleAssignmentController;
+    let controller: VehicleAssignmentController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [VehicleAssignmentController],
-      providers: [VehicleAssignmentService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [VehicleAssignmentController],
+            providers: [
+                {
+                    provide: VehicleAssignmentService,
+                    useValue: mockAssignmentService,
+                },
+            ],
+        }).compile();
 
-    controller = module.get<VehicleAssignmentController>(VehicleAssignmentController);
-  });
+        controller = module.get<VehicleAssignmentController>(
+            VehicleAssignmentController,
+        );
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    describe('1 - register method', () => {
+        it('1.1 - should return a register', async () => {
+            mockAssignmentService.register.mockReturnValueOnce(
+                mockRegisters[0],
+            );
+            const response = await controller.register(mockCreateRegister);
+            expect(response).toStrictEqual(mockRegisters[0]);
+        });
+    });
+
+    describe('2 - registerFinish method', () => {
+        it('2.1 - should return a register', async () => {
+            mockAssignmentService.finish.mockReturnValueOnce(mockRegisters[1]);
+            const response = await controller.registerFinish(
+                mockCreateRegister,
+            );
+            expect(response).toStrictEqual(mockRegisters[1]);
+        });
+    });
+
+    describe('3 - findByVehicleId method', () => {
+        it('3.1 - should return a registers', async () => {
+            mockAssignmentService.findByVehicleId.mockReturnValueOnce(
+                mockRegisters,
+            );
+            const response = await controller.findByVehicleId('1');
+            expect(response).toStrictEqual(mockRegisters);
+        });
+    });
+
+    describe('4 - findByDriverId method', () => {
+        it('4.1 - should return a registers', async () => {
+            mockAssignmentService.findByDriverId.mockReturnValueOnce(
+                mockRegisters,
+            );
+            const response = await controller.findByDriverId('1');
+            expect(response).toStrictEqual(mockRegisters);
+        });
+    });
 });
