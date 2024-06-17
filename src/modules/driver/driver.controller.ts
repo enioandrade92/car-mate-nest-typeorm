6062c014b656dd7bd816ba3c36e552691987fb7b
Enio Andrade <enio.goulart@gmail.com>
2024-06-17 17:07:58 -0300

    fix(driver-controller): added authenticate

--- src/modules/driver/driver.controller.ts
+++ src/modules/driver/driver.controller.ts
@@ -1,47 +1,55 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Param,
-    Delete,
-    Put,
-    Query,
+	Controller,
+	Get,
+	Post,
+	Body,
+	Param,
+	Delete,
+	Put,
+	Query,
+	UseGuards,
 } from '@nestjs/common';
 import { DriverService } from './driver.service';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { FilterDriverDto } from './dto/filter-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UserFromSession } from '../auth/decorators/user-from-session.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('driver')
 export class DriverController {
-    constructor(private readonly driverService: DriverService) {}
+	constructor(private readonly driverService: DriverService) {}
 
-    @Post()
-    async createDriver(@Body() createDriverDto: CreateDriverDto) {
-        return this.driverService.createDriver(createDriverDto);
-    }
+	@Post()
+	async createDriver(
+		@Body() createDriverDto: CreateDriverDto,
+		@UserFromSession('user') user: any,
+	) {
+		console.log(user);
+		return this.driverService.createDriver(createDriverDto);
+	}
 
-    @Put(':id')
-    updateDriver(
-        @Param('id') id: string,
-        @Body() updateDriverDto: UpdateDriverDto,
-    ) {
-        return this.driverService.updateDriver(+id, updateDriverDto);
-    }
+	@Put(':id')
+	updateDriver(
+		@Param('id') id: string,
+		@Body() updateDriverDto: UpdateDriverDto,
+	) {
+		return this.driverService.updateDriver(+id, updateDriverDto);
+	}
 
-    @Get()
-    findDriverByFilters(@Query() queryString: FilterDriverDto) {
-        return this.driverService.findDriverByFilters(queryString);
-    }
+	@Get()
+	findDriverByFilters(@Query() queryString: FilterDriverDto) {
+		return this.driverService.findDriverByFilters(queryString);
+	}
 
-    @Get(':id')
-    findDriverById(@Param('id') id: string) {
-        return this.driverService.findDriverById(+id);
-    }
+	@Get(':id')
+	findDriverById(@Param('id') id: string) {
+		return this.driverService.findDriverById(+id);
+	}
 
-    @Delete(':id')
-    removeDriver(@Param('id') id: string) {
-        return this.driverService.removeDriver(+id);
-    }
+	@Delete(':id')
+	removeDriver(@Param('id') id: string) {
+		return this.driverService.removeDriver(+id);
+	}
 }
